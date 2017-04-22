c0ce5358e1e6cb5dab341d62554b708a4c446a8a
Filip Dechterenko <filip.dechterenko@gmail.com>
2017-04-22 09:23:47 +0200

    Added roxygen comments for all functions

--- R/hooks.R
+++ R/hooks.R
@@ -1,3 +1,12 @@
+#' Function that is run on loading
+#'
+#' @param libname 
+#' @param pkgname 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 .onLoad <- function(libname, pkgname) {
   assign("pkg_globals", new.env(), envir=parent.env(environment()))
   assign("eye-dir", "cache/eye", pkg_globals)
@@ -15,6 +24,16 @@
   assign("tsd", 0.0265, pkg_globals)
   
 }
+#' Function that is run on attaching
+#' @description 
+#' Set default parameters for the package
+#' @param libname 
+#' @param pkgname 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 .onAttach <- function(libname, pkgname) {
   
   assign("eye-dir", "cache/eye", pkg_globals)
@@ -33,6 +52,15 @@
   
 }
 
+#' Function for setting global parameters
+#'
+#' @param l List with individual parameters that will be changed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' scanpatterns.set.parameters(list("max-time" = 40))
 scanpatterns.set.parameters<-function(l) {
   for(i in 1:length(l)) {
     assign(names(l)[i], l[[i]], pkg_globals)
